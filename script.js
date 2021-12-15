cbb8c35fd428bdce7cc27a70f0b6d5dcd590d584
MasthanValiSMD <masthanvalismd@gmail.com>
2021-12-15 18:41:02 +0530

    coffee-api

---
+++ script.js
@@ -0,0 +1,13 @@
+function random() {
+    var url = "https://coffee.alexflipnote.dev/random"
+    window.open(url)
+}
+
+function randomJson() {
+    var link = "https://coffee.alexflipnote.dev/random.json"
+    window.open(link)
+}
+function gitHub() {
+    var gitLink = "https://github.com/AlexFlipnote/CoffeeAPI"
+    window.open(gitLink)
+  }
